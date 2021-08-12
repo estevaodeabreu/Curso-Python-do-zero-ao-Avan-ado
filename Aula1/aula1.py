2824e87adcbaec461c8385e03876ae8321aaf3ea
estevaodeabreu <estevaodeabreu.gti@gmail.com>
2021-08-12 19:56:43 -0300

    commit aula 1 sobre comentarios em codigos e atualização do read me

---
+++ Aula1/aula1.py
@@ -0,0 +1,11 @@
+# ISSO É UM COMENTARIO
+
+print('Assim o codigo roda')  # ctrl + d duplica a linha
+
+# Uma boa pratica é usar 2 espaços separando o comentario do código
+#print('Assim é comentário')
+
+""" tres aspas servem para documentação de codigo
+ou grandes blocos de informação,
+para não precisar por # em cada linha, e ao fim do bloco, 
+feche com as aspas novamente"""
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Curso-Python-do-zero-ao-Avan-ado
+# Curso Python do Zero ao Deploy
+
+#### Curso com logica,POO,Django,Integração com SQL,CRUD,Deploy e ainda cursos bonus de HTML5 e CSS
