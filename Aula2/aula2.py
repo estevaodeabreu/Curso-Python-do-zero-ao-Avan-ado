4f819b7d853067d8e845ec76d49e4deda7b7fcbc
estevaodeabreu <estevaodeabreu.gti@gmail.com>
2021-08-12 20:58:51 -0300

    "parâmetros interessantes que podem ser aplicados a função print"

---
+++ Aula2/aula2.py
@@ -0,0 +1,11 @@
+
+#print(123456)
+#print('O basico precisa ser praticado,''para ser fixado')
+
+print('Estevão', 'de','Abreu', sep='_', end='  ')  # sep, neste caso é separador
+print('Ronaldo', 'L','Nogueira', sep='_')  # end + espaço, neste caso, põe os dois prints na mesma linha
+
+#criar um cpf fake com separador no print:
+
+print('004','004','004',sep='.',end='-')
+print('04')
