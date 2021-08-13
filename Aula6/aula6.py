02a01888ed6c1d8341f9a92b5c993e4b2b3bfb87
estevaodeabreu <estevaodeabreu.gti@gmail.com>
2021-08-13 18:58:17 -0300

    Commit aula 6

---
+++ Aula6/aula6.py
@@ -0,0 +1,15 @@
+'''  Variaveis
+
+regras: Devem começar com uma letra, podem conter numeros, podem conter palavras separadas
+por _ devem ser escritas preferencialmente com letras minusculas. '''
+
+nome = 'Estevão'  # ------- variavel do tipo string
+idade = 32  #------------- variavel do tipo int
+altura = 1.79  #---------- variavel do tipo float
+run = True  #------------- variavel do tipo bool
+peso = 92   #------------- variavel do tipo int
+
+imc = peso / altura ** 2 #- variavel aritimetica
+
+print(nome ,'seu IMC é {} :'.format(imc))
+
